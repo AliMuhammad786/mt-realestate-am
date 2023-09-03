0a9ce8c027caa9a96e84b220827c6015a5aea5e7
AliMuhammad786 <ali_muhammad_2010@yahoo.com>
2023-09-03 16:35:54 +0500

    create the header component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Home from "./pages/Home.jsx";
 import Profile from "./pages/Profile.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
-import ForgotPassword from "./pages/ForgotPassword.jsx"
+import ForgotPassword from "./pages/ForgotPassword.jsx";
 import Offers from "./pages/Offer.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
