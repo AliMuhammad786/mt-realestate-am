7fbfe966ab4b910bb13810cd8f6ff0b4092d03e5
AliMuhammad786 <ali_muhammad_2010@yahoo.com>
2023-09-05 20:52:09 +0500

    create the CreateListing page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,51 +1,50 @@
 import { getAuth, updateProfile } from "firebase/auth";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { doc, updateDoc } from "firebase/firestore"
-import { db } from "../firebase"
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { FcHome } from "react-icons/fc";
 
 export default function Profile() {
-
-  const auth = getAuth()
-  const navigate = useNavigate()
-  const [changeDetail, setChangeDetail] = useState(false)
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [changeDetail, setChangeDetail] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
 
   const { name, email } = formData;
-  function onLogout () {
-    auth.signOut()
-    navigate("/")
-
+  function onLogout() {
+    auth.signOut();
+    navigate("/");
   }
 
   function onChange(e) {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
-    }))
+    }));
   }
 
   async function onSubmit() {
     try {
-      if(auth.currentUser.displayName !== name) {
+      if (auth.currentUser.displayName !== name) {
         // update displayname in firebase authentication
         await updateProfile(auth.currentUser, {
           displayName: name,
         });
 
         // update the name in the firebase
-        const docRef = doc(db, "users", auth.currentUser.uid)
+        const docRef = doc(db, "users", auth.currentUser.uid);
         await updateDoc(docRef, {
           name,
-        })
+        });
       }
-      toast.success("Profile details updated successfully")
-    } catch(error) {
-      toast.error("Could not update the profile details")
+      toast.success("Profile details updated successfully");
+    } catch (error) {
+      toast.error("Could not update the profile details");
     }
   }
 
@@ -64,7 +63,9 @@ export default function Profile() {
               value={name}
               disabled={!changeDetail}
               onChange={onChange}
-              className={`mb-6 w-full px-4 py-2 text-xl text-gray-600 bg-white border border-gray-300 rounded transition ease-in-out ${changeDetail && "bg-red-200 focus:bg-red-200"}`}
+              className={`mb-6 w-full px-4 py-2 text-xl text-gray-600 bg-white border border-gray-300 rounded transition ease-in-out ${
+                changeDetail && "bg-red-200 focus:bg-red-200"
+              }`}
             />
             {/* email input */}
             <input
@@ -77,14 +78,30 @@ export default function Profile() {
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
               <p className="flex items-center">
                 Do you want to change your name?
-                <span onClick={() => {
-                  changeDetail && onSubmit()
-                  setChangeDetail((prevState) => !prevState)
-                }}  className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-2 cursor-pointer">{changeDetail ? "Apply change" : "Edit"}</span>
+                <span
+                  onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }}
+                  className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-2 cursor-pointer"
+                >
+                  {changeDetail ? "Apply change" : "Edit"}
+                </span>
               </p>
-              <p onClick={onLogout} className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer">Sign Out</p>
+              <p
+                onClick={onLogout}
+                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
+              >
+                Sign Out
+              </p>
             </div>
           </form>
+          <button type="submit" className='w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'>
+            <Link to='/create-listing' className="flex justify-center items-center">
+              <FcHome className="mr-2 text-3xl bg-red-200 rounded-full p-1 border-2" />
+              Sell or Rent your home
+            </Link>
+          </button>
         </div>
       </section>
     </>
